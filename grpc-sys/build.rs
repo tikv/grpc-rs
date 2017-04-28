b5e805cf642dbb012615eaf303a0b08e53942eb6
andelf <andelf@gmail.com>
2017-04-28 18:32:18 +0800

    *: address commit

--- grpc-sys/build.rs
+++ grpc-sys/build.rs
@@ -11,35 +11,40 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(dead_code, unused_imports)]
-
 extern crate gcc;
 extern crate cmake;
 extern crate pkg_config;
 
-use std::path::Path;
-use std::process::Command;
-
 const GRPC_VERSION: &'static str = "1.2.5";
-const ZLIB_VERSION: &'static str = "1.2.8";
-const BORINGSSL_GIT_HASH: &'static str = "78684e5b222645828ca302e56b40b9daff2b2d27";
 
 #[cfg(not(feature = "static-link"))]
-fn build_or_link_grpc(cc: &mut gcc::Config) {
-    if let Ok(lib) = pkg_config::Config::new()
-        .atleast_version(GRPC_VERSION)
-        .statik(true)
-        .probe("grpc_unsecure") {
-        for inc_path in lib.include_paths {
-            cc.include(inc_path);
+mod imp {
+    use super::GRPC_VERSION;
+
+    pub fn build_or_link_grpc(cc: &mut ::gcc::Config) {
+        if let Ok(lib) = ::pkg_config::Config::new()
+            .atleast_version(GRPC_VERSION)
+            .statik(true)
+            .probe("grpc_unsecure") {
+            for inc_path in lib.include_paths {
+                cc.include(inc_path);
+            }
+        } else {
+            panic!("can't find dynamic grpc library");
         }
-    } else {
-        panic!("can't find dynamic grpc library");
     }
 }
 
 #[cfg(feature = "static-link")]
-fn build_or_link_grpc(cc: &mut gcc::Config) {
+mod imp {
+    use std::path::Path;
+    use std::process::Command;
+
+    use super::GRPC_VERSION;
+
+    const ZLIB_VERSION: &'static str = "1.2.8";
+    const BORINGSSL_GIT_HASH: &'static str = "78684e5b222645828ca302e56b40b9daff2b2d27";
+
     fn wget(url: &str, out: &str) -> Result<(), String> {
         Command::new("wget")
             .args(&["-q", "-c", "-O", out, url])
@@ -68,58 +73,61 @@ fn build_or_link_grpc(cc: &mut gcc::Config) {
             })
     }
 
-    if !Path::new(&format!("grpc-{}", GRPC_VERSION)).exists() {
-        wget(&format!("https://github.com/grpc/grpc/archive/v{}.tar.gz",
-                      GRPC_VERSION),
-             "grpc.tar.gz")
-            .and_then(|_| tar_xf("grpc.tar.gz"))
-            .unwrap();
-    }
+    pub fn build_or_link_grpc(cc: &mut ::gcc::Config) {
 
-    if !Path::new(&format!("zlib-{}", ZLIB_VERSION)).exists() {
-        wget(&format!("https://github.com/madler/zlib/archive/v{}.tar.gz",
-                      ZLIB_VERSION),
-             "zlib.tar.gz")
-            .and_then(|_| tar_xf("zlib.tar.gz"))
-            .unwrap();
-    }
+        if !Path::new(&format!("grpc-{}", GRPC_VERSION)).exists() {
+            wget(&format!("https://github.com/grpc/grpc/archive/v{}.tar.gz",
+                          GRPC_VERSION),
+                 "grpc.tar.gz")
+                .and_then(|_| tar_xf("grpc.tar.gz"))
+                .unwrap();
+        }
 
-    if !Path::new(&format!("boringssl-{}", BORINGSSL_GIT_HASH)).exists() {
-        wget(&format!("https://github.com/google/boringssl/archive/{}.tar.gz",
-                      BORINGSSL_GIT_HASH),
-             "boringssl.tar.gz")
-            .and_then(|_| tar_xf("boringssl.tar.gz"))
-            .unwrap();
-    }
+        if !Path::new(&format!("zlib-{}", ZLIB_VERSION)).exists() {
+            wget(&format!("https://github.com/madler/zlib/archive/v{}.tar.gz",
+                          ZLIB_VERSION),
+                 "zlib.tar.gz")
+                .and_then(|_| tar_xf("zlib.tar.gz"))
+                .unwrap();
+        }
 
-    let dst = cmake::Config::new(format!("zlib-{}", ZLIB_VERSION)).build();
+        if !Path::new(&format!("boringssl-{}", BORINGSSL_GIT_HASH)).exists() {
+            wget(&format!("https://github.com/google/boringssl/archive/{}.tar.gz",
+                          BORINGSSL_GIT_HASH),
+                 "boringssl.tar.gz")
+                .and_then(|_| tar_xf("boringssl.tar.gz"))
+                .unwrap();
+        }
 
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+        let dst = ::cmake::Config::new(format!("zlib-{}", ZLIB_VERSION)).build();
 
-    let dst = cmake::Config::new(format!("grpc-{}", GRPC_VERSION))
+        println!("cargo:rustc-link-search=native={}/lib", dst.display());
+
+        let dst = ::cmake::Config::new(format!("grpc-{}", GRPC_VERSION))
         .define("ZLIB_ROOT_DIR", format!("../zlib-{}", ZLIB_VERSION)) // relative to grpc dir
         .define("BORINGSSL_ROOT_DIR", format!("../boringssl-{}", BORINGSSL_GIT_HASH))
         .build_target("grpc_unsecure")
         .build();
 
-    cc.include(format!("grpc-{}/include", GRPC_VERSION));
+        cc.include(format!("grpc-{}/include", GRPC_VERSION));
 
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
-    println!("cargo:rustc-link-search=native={}/build/third_party/zlib",
-             dst.display());
+        println!("cargo:rustc-link-search=native={}/build", dst.display());
+        println!("cargo:rustc-link-search=native={}/build/third_party/zlib",
+                 dst.display());
 
-    println!("cargo:rustc-link-lib=static=z");
-    println!("cargo:rustc-link-lib=static=gpr");
-    println!("cargo:rustc-link-lib=static=grpc_unsecure");
+        println!("cargo:rustc-link-lib=static=z");
+        println!("cargo:rustc-link-lib=static=gpr");
+        println!("cargo:rustc-link-lib=static=grpc_unsecure");
+    }
 }
 
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
-    let mut cc = gcc::Config::new();
+    let mut cc = ::gcc::Config::new();
 
-    build_or_link_grpc(&mut cc);
+    imp::build_or_link_grpc(&mut cc);
 
     cc.file("grpc_wrap.c")
         .flag("-fPIC")
