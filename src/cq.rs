088ac6958ba3605bf8b1f7bd0e4212ad88bba9c7
Jay Lee <busyjaylee@gmail.com>
2017-05-02 23:24:35 +0800

    polish completion queue and environ

--- src/cq.rs
+++ src/cq.rs
@@ -1,11 +1,25 @@
+// Copyright 2017 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 
-use grpc_sys::{self, GprClockType, GrpcCompletionQueue};
-pub use grpc_sys::GrpcCompletionType as EventType;
-
-pub use grpc_sys::GrpcEvent as Event;
 use std::ptr;
 
+use grpc_sys::{self, GprClockType, GrpcCompletionQueue};
+
+pub use grpc_sys::GrpcCompletionType as EventType;
+pub use grpc_sys::GrpcEvent as Event;
+
+/// `CompletionQueue` enable notification of the completion of asynchronous actions.
 pub struct CompletionQueue {
     cq: *mut GrpcCompletionQueue,
 }
@@ -18,6 +32,7 @@ impl CompletionQueue {
         CompletionQueue { cq: unsafe { grpc_sys::grpc_completion_queue_create(ptr::null_mut()) } }
     }
 
+    /// Blocks until an event is available, the completion queue is being shut down.
     pub fn next(&self) -> Event {
         unsafe {
             let inf = grpc_sys::gpr_inf_future(GprClockType::Realtime);
@@ -25,6 +40,10 @@ impl CompletionQueue {
         }
     }
 
+    /// Begin destruction of a completion queue.
+    ///
+    /// Once all possible events are drained then `next()` will start to produce
+    /// `Event::QueueShutdown` events only.
     pub fn shutdown(&self) {
         unsafe {
             grpc_sys::grpc_completion_queue_shutdown(self.cq);
--- src/env.rs
+++ src/env.rs
@@ -1,16 +1,33 @@
+// Copyright 2017 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::thread::{Builder, JoinHandle};
+
+use grpc_sys;
 
 use async::Promise;
 use cq::{CompletionQueue, EventType};
 
-use grpc_sys;
-use std::sync::Arc;
-use std::thread::{Builder, JoinHandle};
-
+// event loop
 fn poll_queue(cq: Arc<CompletionQueue>) {
     loop {
         let e = cq.next();
         match e.event_type {
             EventType::QueueShutdown => break,
+            // timeout should not happen in theory.
             EventType::QueueTimeout => continue,
             EventType::OpComplete => {}
         }
@@ -21,12 +38,17 @@ fn poll_queue(cq: Arc<CompletionQueue>) {
     }
 }
 
+/// An object that used to control concurrency and start event loop.
 pub struct Environment {
     cqs: Vec<Arc<CompletionQueue>>,
+    idx: AtomicUsize,
     _handles: Vec<JoinHandle<()>>,
 }
 
 impl Environment {
+    /// Initialize grpc and create a threadpool to poll event loop.
+    ///
+    /// Each thread in threadpool will has one event loop.
     pub fn new(cq_count: usize) -> Environment {
         assert!(cq_count > 0);
         unsafe {
@@ -47,16 +69,44 @@ impl Environment {
 
         Environment {
             cqs: cqs,
+            idx: AtomicUsize::new(0),
             _handles: handles,
         }
     }
 
+    /// Get all the created completion queues.
     pub fn completion_queues(&self) -> &[Arc<CompletionQueue>] {
         self.cqs.as_slice()
     }
 
+    /// Pick an arbitrary completion queue.
     pub fn pick_a_cq(&self) -> Arc<CompletionQueue> {
-        // TODO: randomly pick up
-        self.cqs[0].clone()
+        let idx = self.idx.fetch_add(1, Ordering::Relaxed);
+        self.cqs[idx % self.cqs.len()].clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic_loop() {
+        let env = Environment::new(2);
+
+        let q1_ptr = env.pick_a_cq();
+        let q2_ptr = env.pick_a_cq();
+        let q3_ptr = env.pick_a_cq();
+        assert_eq!(q1_ptr.as_ptr(), q3_ptr.as_ptr());
+        assert_ne!(q1_ptr.as_ptr(), q2_ptr.as_ptr());
+
+        assert_eq!(env.completion_queues().len(), 2);
+        for cq in env.completion_queues() {
+            cq.shutdown();
+        }
+
+        for handle in env._handles {
+            handle.join().unwrap();
+        }
     }
 }
