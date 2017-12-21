c869965fa4b07425f78c038a36e34c0be3bc1c15
functor <meehow@gmail.com>
2017-12-21 16:09:32 -0500

    Add a verion of the hello_world example modified to also use the metadata functionality.

---
+++ examples/hello_world_metadata/server.rs
@@ -0,0 +1,64 @@
+#![allow(unknown_lints)]
+#![allow(unreadable_literal)]
+
+extern crate futures;
+extern crate grpcio;
+extern crate grpcio_proto;
+#[macro_use]
+extern crate log;
+
+#[path = "../log_util.rs"]
+mod log_util;
+
+use std::io::Read;
+use std::sync::Arc;
+use std::{io, thread};
+
+use futures::Future;
+use futures::sync::oneshot;
+use grpcio::{Environment, RpcContext, ServerBuilder, UnarySink};
+
+use grpcio_proto::example::helloworld::{HelloReply, HelloRequest};
+use grpcio_proto::example::helloworld_grpc::{self, Greeter};
+
+#[derive(Clone)]
+struct GreeterService;
+
+impl Greeter for GreeterService {
+    fn say_hello(&self, ctx: RpcContext, req: HelloRequest, sink: UnarySink<HelloReply>) {
+        for index in 0..ctx.metadata().count() {
+            let key = std::str::from_utf8(ctx.metadata().key(index)).expect("key");
+            let value = std::str::from_utf8(ctx.metadata().value(index)).expect("value");
+            info!("metadata entry {}:  {} -> {} ", index, key, value);
+        }
+        let msg = format!("Hello {}", req.get_name());
+        let mut resp = HelloReply::new();
+        resp.set_message(msg);
+        let f = sink.success(resp)
+            .map_err(move |e| error!("failed to reply {:?}: {:?}", req, e));
+        ctx.spawn(f)
+    }
+}
+
+fn main() {
+    let _guard = log_util::init_log(None);
+    let env = Arc::new(Environment::new(1));
+    let service = helloworld_grpc::create_greeter(GreeterService);
+    let mut server = ServerBuilder::new(env)
+        .register_service(service)
+        .bind("127.0.0.1", 50051)
+        .build()
+        .unwrap();
+    server.start();
+    for &(ref host, port) in server.bind_addrs() {
+        info!("listening on {}:{}", host, port);
+    }
+    let (tx, rx) = oneshot::channel();
+    thread::spawn(move || {
+        info!("Press ENTER to exit...");
+        let _ = io::stdin().read(&mut [0]).unwrap();
+        tx.send(())
+    });
+    let _ = rx.wait();
+    let _ = server.shutdown().wait();
+}
