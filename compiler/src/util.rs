b42b46cad046f8e13dbddac852d46f5721be0423
Jay <BusyJay@users.noreply.github.com>
2018-02-09 13:34:31 +0800

    format files with rustfmt 0.3.4-nightly (#157)

--- compiler/src/util.rs
+++ compiler/src/util.rs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 use std::str;
 
 // A struct that divide a name into serveral parts that meets rust's guidelines.
