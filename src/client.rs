45bddea311a5a4751092976ede6a301abd94720a
Wenxuan Shi <hi@breeswish.org>
2018-07-18 15:12:31 +0800

    Add verbose documentations (#204)

--- src/client.rs
+++ src/client.rs
@@ -21,52 +21,53 @@ use channel::Channel;
 
 use error::Result;
 
-/// A generic client for making rpc calls.
+/// A generic client for making RPC calls.
 pub struct Client {
     channel: Channel,
 }
 
 impl Client {
+    /// Initialize a new [`Client`].
     pub fn new(channel: Channel) -> Client {
         Client { channel }
     }
 
-    /// Create a synchronized unary rpc call.
-    pub fn unary_call<P, Q>(&self, method: &Method<P, Q>, req: &P, opt: CallOption) -> Result<Q> {
+    /// Create a synchronized unary RPC call.
+    pub fn unary_call<Req, Resp>(&self, method: &Method<Req, Resp>, req: &Req, opt: CallOption) -> Result<Resp> {
         let f = self.unary_call_async(method, req, opt)?;
         f.wait()
     }
 
-    /// Create a asynchronized unary rpc call.
-    pub fn unary_call_async<P, Q>(
+    /// Create an asynchronized unary RPC call.
+    pub fn unary_call_async<Req, Resp>(
         &self,
-        method: &Method<P, Q>,
-        req: &P,
+        method: &Method<Req, Resp>,
+        req: &Req,
         opt: CallOption,
-    ) -> Result<ClientUnaryReceiver<Q>> {
+    ) -> Result<ClientUnaryReceiver<Resp>> {
         Call::unary_async(&self.channel, method, req, opt)
     }
 
-    /// Create a asynchronized client streaming call.
+    /// Create an asynchronized client streaming call.
     ///
     /// Client can send a stream of requests and server responds with a single response.
-    pub fn client_streaming<P, Q>(
+    pub fn client_streaming<Req, Resp>(
         &self,
-        method: &Method<P, Q>,
+        method: &Method<Req, Resp>,
         opt: CallOption,
-    ) -> Result<(ClientCStreamSender<P>, ClientCStreamReceiver<Q>)> {
+    ) -> Result<(ClientCStreamSender<Req>, ClientCStreamReceiver<Resp>)> {
         Call::client_streaming(&self.channel, method, opt)
     }
 
-    /// Create a asynchronized server streaming call.
+    /// Create an asynchronized server streaming call.
     ///
     /// Client sends on request and server responds with a stream of responses.
-    pub fn server_streaming<P, Q>(
+    pub fn server_streaming<Req, Resp>(
         &self,
-        method: &Method<P, Q>,
-        req: &P,
+        method: &Method<Req, Resp>,
+        req: &Req,
         opt: CallOption,
-    ) -> Result<ClientSStreamReceiver<Q>> {
+    ) -> Result<ClientSStreamReceiver<Resp>> {
         Call::server_streaming(&self.channel, method, req, opt)
     }
 
@@ -75,15 +76,15 @@ impl Client {
     /// Client sends a stream of requests and server responds with a stream of responses.
     /// The response stream is completely independent and both side can be sending messages
     /// at the same time.
-    pub fn duplex_streaming<P, Q>(
+    pub fn duplex_streaming<Req, Resp>(
         &self,
-        method: &Method<P, Q>,
+        method: &Method<Req, Resp>,
         opt: CallOption,
-    ) -> Result<(ClientDuplexSender<P>, ClientDuplexReceiver<Q>)> {
+    ) -> Result<(ClientDuplexSender<Req>, ClientDuplexReceiver<Resp>)> {
         Call::duplex_streaming(&self.channel, method, opt)
     }
 
-    /// Spawn the future into current grpc poll thread.
+    /// Spawn the future into current gRPC poll thread.
     ///
     /// This can reduce a lot of context switching, but please make
     /// sure there is no heavy work in the future.
