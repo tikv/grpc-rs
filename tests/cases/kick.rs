634560e1a730b26947d77b4b66a7336ba57e7e3d
Neil Shen <overvenus@gmail.com>
2018-09-06 21:22:34 +0800

    Kick completion queue via call (#217)

--- tests/cases/kick.rs
+++ tests/cases/kick.rs
@@ -47,7 +47,7 @@ impl Greeter for GreeterService {
 }
 
 #[test]
-fn test_alarm_notify() {
+fn test_kick() {
     let env = Arc::new(EnvBuilder::new().build());
     let tx = Arc::new(Mutex::new(None));
     let service = create_greeter(GreeterService { tx: tx.clone() });
@@ -74,4 +74,31 @@ fn test_alarm_notify() {
     }
     let reply = f.wait().expect("rpc");
     assert_eq!(reply.get_message(), "hello world");
+
+    // Spawn a future in the client.
+    let (tx1, rx2) = spawn_chianed_channel(&client);
+    thread::sleep(Duration::from_millis(10));
+    let _ = tx1.send(77);
+    assert_eq!(rx2.wait().unwrap(), 77);
+
+    // Drop the client before a future is resloved.
+    let (tx1, rx2) = spawn_chianed_channel(&client);
+    drop(client);
+    thread::sleep(Duration::from_millis(10));
+    let _ = tx1.send(88);
+    assert_eq!(rx2.wait().unwrap(), 88);
+}
+
+fn spawn_chianed_channel(
+    client: &GreeterClient,
+) -> (oneshot::Sender<usize>, oneshot::Receiver<usize>) {
+    let (tx1, rx1) = oneshot::channel();
+    let (tx2, rx2) = oneshot::channel();
+    let f =
+        rx1.map(|n| {
+            let _ = tx2.send(n);
+        }).map_err(|_| ());
+    client.spawn(f);
+
+    (tx1, rx2)
 }
