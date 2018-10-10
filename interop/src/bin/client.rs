668fc98f407655967d19cb29840c4f69b95fb935
A. Hobden <andrew@hoverbear.org>
2018-10-10 01:17:56 -0700

    Use stable clippy (#230)

--- interop/src/bin/client.rs
+++ interop/src/bin/client.rs
@@ -32,38 +32,32 @@ fn main() {
                 .long("server_host")
                 .help("The server host to connect to. For example, \"localhost\" or \"127.0.0.1\"")
                 .takes_value(true),
-        )
-        .arg(
+        ).arg(
             Arg::with_name("host_override")
                 .long("server_host_override")
                 .help("The server host to connect to. For example, \"localhost\" or \"127.0.0.1\"")
                 .takes_value(true),
-        )
-        .arg(
+        ).arg(
             Arg::with_name("port")
                 .long("server_port")
                 .help("The server port to connect to. For example, \"8080\"")
                 .takes_value(true),
-        )
-        .arg(
+        ).arg(
             Arg::with_name("case")
                 .long("test_case")
                 .help("The name of the test case to execute. For example, \"empty_unary\"")
                 .takes_value(true),
-        )
-        .arg(
+        ).arg(
             Arg::with_name("use_tls")
                 .long("use_tls")
                 .help("Whether to use a plaintext or encrypted connection")
                 .takes_value(true),
-        )
-        .arg(
+        ).arg(
             Arg::with_name("use_test_ca")
                 .long("use_test_ca")
                 .help("Whether to replace platform root CAs with ca.pem as the CA root")
                 .takes_value(true),
-        )
-        .get_matches();
+        ).get_matches();
     let host = matches.value_of("host").unwrap_or("127.0.0.1");
     let host_override = matches
         .value_of("host_override")
