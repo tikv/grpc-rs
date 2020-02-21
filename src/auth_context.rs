abdd193c536d6e177be36d8a6f8236ec782caac1
Thomas BESSOU <thomas.bessou@hotmail.fr>
2020-02-21 12:27:48 +0100

    Wrap `grpc_auth_context` in NonNull and remove asserts
    Signed-off-by: Thomas BESSOU <thomas.bessou@hotmail.fr>

--- src/auth_context.rs
+++ src/auth_context.rs
@@ -3,6 +3,7 @@
 
 use std::ffi::CStr;
 use std::marker::PhantomData;
+use std::ptr::NonNull;
 
 use crate::grpc_sys::{
     self, grpc_auth_context, grpc_auth_property, grpc_auth_property_iterator, grpc_call,
@@ -23,26 +24,20 @@ use crate::grpc_sys::{
 /// identity (e.g. for client certificate authentication this property will be
 /// `x509_common_name` or `x509_subject_alternative_name`).
 pub struct AuthContext {
-    ctx: *mut grpc_auth_context,
+    ctx: NonNull<grpc_auth_context>,
 }
 
 /// Binding to gRPC Core AuthContext
 impl AuthContext {
     pub(crate) unsafe fn from_call_ptr(call: *mut grpc_call) -> Option<Self> {
-        let ctx = grpc_sys::grpc_call_auth_context(call);
-        if ctx.is_null() {
-            None
-        } else {
-            Some(AuthContext { ctx })
-        }
+        NonNull::new(grpc_sys::grpc_call_auth_context(call)).map(|ctx| AuthContext { ctx })
     }
 
     /// The name of the property gRPC Core has chosen as main peer identity property,
     /// if any.
     pub fn peer_identity_property_name(&self) -> Option<&str> {
-        assert!(!self.ctx.is_null());
         unsafe {
-            let p = grpc_sys::grpc_auth_context_peer_identity_property_name(self.ctx);
+            let p = grpc_sys::grpc_auth_context_peer_identity_property_name(self.ctx.as_ref());
             if p.is_null() {
                 None
             } else {
@@ -55,18 +50,16 @@ impl AuthContext {
     /// considered valid by gRPC).
     /// `false` in non-secure scenarios.
     pub fn peer_is_authenticated(&self) -> bool {
-        assert!(!self.ctx.is_null());
-        unsafe { grpc_sys::grpc_auth_context_peer_is_authenticated(self.ctx) != 0 }
+        unsafe { grpc_sys::grpc_auth_context_peer_is_authenticated(self.ctx.as_ref()) != 0 }
     }
 
     /// `AuthContext[peer_identity_property_name()]`
     ///
     /// There may be several of them (for instance if `x509_subject_alternative_name` is selected)
     pub fn peer_identity(&self) -> AuthPropertyIter {
-        assert!(!self.ctx.is_null());
         unsafe {
             // grpc_auth_context_peer_identity returns empty_iterator when self.ctx is NULL
-            let iter = grpc_sys::grpc_auth_context_peer_identity(self.ctx);
+            let iter = grpc_sys::grpc_auth_context_peer_identity(self.ctx.as_ref());
             AuthPropertyIter {
                 iter,
                 _lifetime: PhantomData,
@@ -83,7 +76,7 @@ impl<'a> IntoIterator for &'a AuthContext {
     fn into_iter(self) -> Self::IntoIter {
         unsafe {
             // grpc_auth_context_property_iterator returns empty_iterator when self.ctx is NULL
-            let iter = grpc_sys::grpc_auth_context_property_iterator(self.ctx);
+            let iter = grpc_sys::grpc_auth_context_property_iterator(self.ctx.as_ref());
             AuthPropertyIter {
                 iter,
                 _lifetime: PhantomData,
@@ -94,7 +87,7 @@ impl<'a> IntoIterator for &'a AuthContext {
 
 impl Drop for AuthContext {
     fn drop(&mut self) {
-        unsafe { grpc_sys::grpc_auth_context_release(self.ctx) }
+        unsafe { grpc_sys::grpc_auth_context_release(self.ctx.as_ptr()) }
     }
 }
 
