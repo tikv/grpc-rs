4a349f4435e4b87f736a1e93ad500a9a3b437028
Xintao <hunterlxt@live.com>
2020-06-04 00:34:28 +0800

    update reset submodule
    Signed-off-by: Xintao <hunterlxt@live.com>

--- scripts/reset-submodule.cmd
+++ scripts/reset-submodule.cmd
@@ -1,7 +1,8 @@
 git submodule update --init grpc-sys/grpc
 cd grpc-sys/grpc
-git submodule update --init third_party/boringssl
+git submodule update --init third_party/boringssl-with-bazel
 git submodule update --init third_party/cares/cares
+git submodule update --init third_party/abseil-cpp
 cd third_party/zlib
 git clean -f
 git reset --hard
