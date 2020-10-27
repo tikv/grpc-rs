4d331af3a3a04d7313df42635d876808ca65bdf8
Jay <BusyJayLee@gmail.com>
2020-10-27 12:18:20 +0800

    fix reset scripts
    Signed-off-by: Jay <BusyJayLee@gmail.com>

--- scripts/reset-submodule.cmd
+++ scripts/reset-submodule.cmd
@@ -3,6 +3,7 @@ cd grpc-sys/grpc
 git submodule update --init third_party/cares/cares
 git submodule update --init third_party/abseil-cpp
 git submodule update --init third_party/re2
+rm -rf third_party/boringssl-with-bazel/*
 cd third_party/zlib
-git clean -f
+git clean -df
 git reset --hard
