1af4693dd52495bd426e960a47c37fd90845d64c
Jay Lee <BusyJayLee@gmail.com>
2021-02-19 14:02:27 +0800

    use provided certs
    Signed-off-by: Jay Lee <BusyJayLee@gmail.com>

--- tests-and-examples/src/util.rs
+++ tests-and-examples/src/util.rs
@@ -3,16 +3,24 @@
 use std::fs;
 use std::io::{self, Read};
 
+const CERT_DIR: &str = "../grpc-sys/grpc/src/core/tsi/test_creds";
+
+fn cert_path(name: &str, ext: &str) -> String {
+    let p = format!("{}/{}.{}", CERT_DIR, name, ext);
+    println!("reading {}", p);
+    p
+}
+
 pub fn read_single_crt(name: &str) -> Result<String, io::Error> {
     let mut crt = String::new();
-    fs::File::open(format!("certs/{}.crt", name))?.read_to_string(&mut crt)?;
+    fs::File::open(cert_path(name, "pem"))?.read_to_string(&mut crt)?;
     Ok(crt)
 }
 
 pub fn read_cert_pair(name: &str) -> Result<(String, String), io::Error> {
     let mut crt = String::new();
     let mut key = String::new();
-    fs::File::open(format!("certs/{}.crt", name))?.read_to_string(&mut crt)?;
-    fs::File::open(format!("certs/{}.key", name))?.read_to_string(&mut key)?;
+    fs::File::open(cert_path(name, "pem"))?.read_to_string(&mut crt)?;
+    fs::File::open(cert_path(name, "key"))?.read_to_string(&mut key)?;
     Ok((crt, key))
 }
