ee85deb14bbc443700e24bd4ac18b98e120ed03c
Christian Oudard <christian.oudard@gmail.com>
2021-10-20 12:23:32 -0700

    Cargo fmt again.
    Signed-off-by: Christian Oudard <christian.oudard@gmail.com>

--- health/src/proto/protobuf/health_grpc.rs
+++ health/src/proto/protobuf/health_grpc.rs
@@ -4,7 +4,6 @@
 // https://github.com/Manishearth/rust-clippy/issues/702
 #![allow(unknown_lints)]
 #![allow(clippy::all)]
-
 #![allow(box_pointers)]
 #![allow(dead_code)]
 #![allow(missing_docs)]
@@ -16,18 +15,36 @@
 #![allow(unused_imports)]
 #![allow(unused_results)]
 
-const METHOD_HEALTH_CHECK: ::grpcio::Method<super::health::HealthCheckRequest, super::health::HealthCheckResponse> = ::grpcio::Method {
+const METHOD_HEALTH_CHECK: ::grpcio::Method<
+    super::health::HealthCheckRequest,
+    super::health::HealthCheckResponse,
+> = ::grpcio::Method {
     ty: ::grpcio::MethodType::Unary,
     name: "/grpc.health.v1.Health/Check",
-    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    req_mar: ::grpcio::Marshaller {
+        ser: ::grpcio::pb_ser,
+        de: ::grpcio::pb_de,
+    },
+    resp_mar: ::grpcio::Marshaller {
+        ser: ::grpcio::pb_ser,
+        de: ::grpcio::pb_de,
+    },
 };
 
-const METHOD_HEALTH_WATCH: ::grpcio::Method<super::health::HealthCheckRequest, super::health::HealthCheckResponse> = ::grpcio::Method {
+const METHOD_HEALTH_WATCH: ::grpcio::Method<
+    super::health::HealthCheckRequest,
+    super::health::HealthCheckResponse,
+> = ::grpcio::Method {
     ty: ::grpcio::MethodType::ServerStreaming,
     name: "/grpc.health.v1.Health/Watch",
-    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    req_mar: ::grpcio::Marshaller {
+        ser: ::grpcio::pb_ser,
+        de: ::grpcio::pb_de,
+    },
+    resp_mar: ::grpcio::Marshaller {
+        ser: ::grpcio::pb_ser,
+        de: ::grpcio::pb_de,
+    },
 };
 
 #[derive(Clone)]
@@ -42,39 +59,73 @@ impl HealthClient {
         }
     }
 
-    pub fn check_opt(&self, req: &super::health::HealthCheckRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::health::HealthCheckResponse> {
+    pub fn check_opt(
+        &self,
+        req: &super::health::HealthCheckRequest,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<super::health::HealthCheckResponse> {
         self.client.unary_call(&METHOD_HEALTH_CHECK, req, opt)
     }
 
-    pub fn check(&self, req: &super::health::HealthCheckRequest) -> ::grpcio::Result<super::health::HealthCheckResponse> {
+    pub fn check(
+        &self,
+        req: &super::health::HealthCheckRequest,
+    ) -> ::grpcio::Result<super::health::HealthCheckResponse> {
         self.check_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn check_async_opt(&self, req: &super::health::HealthCheckRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::health::HealthCheckResponse>> {
+    pub fn check_async_opt(
+        &self,
+        req: &super::health::HealthCheckRequest,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::health::HealthCheckResponse>> {
         self.client.unary_call_async(&METHOD_HEALTH_CHECK, req, opt)
     }
 
-    pub fn check_async(&self, req: &super::health::HealthCheckRequest) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::health::HealthCheckResponse>> {
+    pub fn check_async(
+        &self,
+        req: &super::health::HealthCheckRequest,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::health::HealthCheckResponse>> {
         self.check_async_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn watch_opt(&self, req: &super::health::HealthCheckRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientSStreamReceiver<super::health::HealthCheckResponse>> {
+    pub fn watch_opt(
+        &self,
+        req: &super::health::HealthCheckRequest,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<::grpcio::ClientSStreamReceiver<super::health::HealthCheckResponse>> {
         self.client.server_streaming(&METHOD_HEALTH_WATCH, req, opt)
     }
 
-    pub fn watch(&self, req: &super::health::HealthCheckRequest) -> ::grpcio::Result<::grpcio::ClientSStreamReceiver<super::health::HealthCheckResponse>> {
+    pub fn watch(
+        &self,
+        req: &super::health::HealthCheckRequest,
+    ) -> ::grpcio::Result<::grpcio::ClientSStreamReceiver<super::health::HealthCheckResponse>> {
         self.watch_opt(req, ::grpcio::CallOption::default())
     }
-    pub fn spawn<F>(&self, f: F) where F: ::futures::Future<Output = ()> + Send + 'static {
+    pub fn spawn<F>(&self, f: F)
+    where
+        F: ::futures::Future<Output = ()> + Send + 'static,
+    {
         self.client.spawn(f)
     }
 }
 
 pub trait Health {
-    fn check(&mut self, ctx: ::grpcio::RpcContext, _req: super::health::HealthCheckRequest, sink: ::grpcio::UnarySink<super::health::HealthCheckResponse>) {
+    fn check(
+        &mut self,
+        ctx: ::grpcio::RpcContext,
+        _req: super::health::HealthCheckRequest,
+        sink: ::grpcio::UnarySink<super::health::HealthCheckResponse>,
+    ) {
         grpcio::unimplemented_call!(ctx, sink)
     }
-    fn watch(&mut self, ctx: ::grpcio::RpcContext, _req: super::health::HealthCheckRequest, sink: ::grpcio::ServerStreamingSink<super::health::HealthCheckResponse>) {
+    fn watch(
+        &mut self,
+        ctx: ::grpcio::RpcContext,
+        _req: super::health::HealthCheckRequest,
+        sink: ::grpcio::ServerStreamingSink<super::health::HealthCheckResponse>,
+    ) {
         grpcio::unimplemented_call!(ctx, sink)
     }
 }
