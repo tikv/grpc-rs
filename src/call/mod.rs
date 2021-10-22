7c1cd6d6729f31dcff50b994b9fbc02bfd4c3dad
Christian Oudard <christian.oudard@gmail.com>
2021-10-22 13:41:14 -0700

    Another cargo clippy fix.
    Signed-off-by: Christian Oudard <christian.oudard@gmail.com>

--- src/call/mod.rs
+++ src/call/mod.rs
@@ -818,7 +818,7 @@ impl SinkBase {
         assert!(self.batch_f.is_none());
 
         let buffer = &mut self.buffer;
-        let mut flags = self.buf_flags.clone().unwrap();
+        let mut flags = self.buf_flags.unwrap();
         flags = flags.buffer_hint(buffer_hint);
 
         let headers = if self.send_metadata {
