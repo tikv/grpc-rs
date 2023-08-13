cc5d2ab4fa0a110d0fdaa49f12552d28a0348721
fortime <palfortime@gmail.com>
2023-08-13 16:49:01 +0800

    fix link error on os with systemd (#629)
    Signed-off-by: fortime <palfortime@gmail.com>

--- grpc-sys/build.rs
+++ grpc-sys/build.rs
@@ -293,7 +293,36 @@ fn build_grpc(cc: &mut cc::Build, library: &str) {
         }
     }
 
+    figure_systemd_path(&build_dir);
+
     cc.include("grpc/include");
+}
+
+fn figure_systemd_path(build_dir: &str) {
+    let path = format!("{build_dir}/CMakeCache.txt");
+    let f = BufReader::new(std::fs::File::open(&path).unwrap());
+    let mut libdir: Option<String> = None;
+    let mut libname: Option<String> = None;
+    for l in f.lines() {
+        let l = l.unwrap();
+        if let Some(s) = trim_start(&l, "SYSTEMD_LIBDIR:INTERNAL=").filter(|s| !s.is_empty()) {
+            libdir = Some(s.to_owned());
+            if libname.is_some() {
+                break;
+            }
+        } else if let Some(s) =
+            trim_start(&l, "SYSTEMD_LIBRARIES:INTERNAL=").filter(|s| !s.is_empty())
+        {
+            libname = Some(s.to_owned());
+            if libdir.is_some() {
+                break;
+            }
+        }
+    }
+    if let (Some(libdir), Some(libname)) = (libdir, libname) {
+        println!("cargo:rustc-link-search=native={}", libdir);
+        println!("cargo:rustc-link-lib={}", libname);
+    }
 }
 
 fn figure_ssl_path(build_dir: &str) {
